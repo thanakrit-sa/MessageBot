222ed85df22dc8f4a76d6aab6dffe28f22cc0c03
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 15:25:29 +0700

    now

--- webhook.php
+++ webhook.php
@@ -10,7 +10,7 @@ $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 
-if($messageText == $messageText){
+if($messageText == "blog"){
     $answer = ["attachment"=>[
      "type"=>"template",
      "payload"=>[
