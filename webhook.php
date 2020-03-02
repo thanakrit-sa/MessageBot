772bf7f303b5f5171f5c9d06e4f47c273d95f6cf
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 11:47:03 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,36 +1,43 @@
 <?php
-$hubVerifyToken = 'cloudwaysschool';
-$accessToken =   "EAAGy9CJyURMBANahP7VfRhZBLCUPlpXHOIMNlg0kfaQptHsqRsfSgCMLauoNcKRa8xBNz5SrdABpRItJCb5fH2fLYOdUux2TF3TC48XD0Bx9VwPwpyDDylkP9y9RqZAVt2reEl9ASIqupZBZAEYbKaEAxFmAJzZAWDE6VzxAjHfxBz6sZBF0bj";
 
-// check token at setup
-if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-  echo $_REQUEST['hub_challenge'];
-  exit;
+/* validate verify token needed for setting up web hook */ 
+if (isset($_GET['hub_verify_token'])) { 
+    if ($_GET['hub_verify_token'] === 'c4d2bc149e31843a21aea7c9db0fd840') {
+        echo $_GET['hub_challenge'];
+        return;
+    } else {
+        echo 'Invalid Verify Token';
+        return;
+    }
 }
 
-// handle bot's anwser
+/* receive and send messages */
 $input = json_decode(file_get_contents('php://input'), true);
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 
-$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$response = null;
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
+    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
 
-//set Message
-if($messageText == "hi") {
-    $answer = "Hello";
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAGy9CJyURMBANahP7VfRhZBLCUPlpXHOIMNlg0kfaQptHsqRsfSgCMLauoNcKRa8xBNz5SrdABpRItJCb5fH2fLYOdUux2TF3TC48XD0Bx9VwPwpyDDylkP9y9RqZAVt2reEl9ASIqupZBZAEYbKaEAxFmAJzZAWDE6VzxAjHfxBz6sZBF0bj';
+
+    /*initialize curl*/
+    $ch = curl_init($url);
+    /*prepare response*/
+    $jsonData = '{
+    "recipient":{
+        "id":"' . $sender . '"
+        },
+        "message":{
+            "text":"You said, ' . $message . '"
+        }
+    }';
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    if (!empty($message)) {
+        $result = curl_exec($ch); // user will get the message
+    }
 }
 
-//send message to facebook bot
-$response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
-];
-
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-$result = curl_exec($ch);
-}
-curl_close($ch);
+?>
