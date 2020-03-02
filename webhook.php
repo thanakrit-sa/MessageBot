5f0632de1f956d159034242f63c35003da2c996d
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 14:03:05 +0700

    now

--- webhook.php
+++ webhook.php
@@ -14,9 +14,65 @@ $response = null;
 if($messageText == "สวัสดีครับ") {
     $answer = "สวัสดี";
 }
-// else {
-//     $answer = "ไม่เข้าใจ";
-// }
+if($messageText == "blog"){
+    $answer = ["attachment"=>[
+     "type"=>"template",
+     "payload"=>[
+       "template_type"=>"generic",
+       "elements"=>[
+         [
+           "title"=>"Welcome to Peter\'s Hats",
+           "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+           "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+           "subtitle"=>"We\'ve got the right hat for everyone.",
+           "buttons"=>[
+             [
+               "type"=>"web_url",
+               "url"=>"https://petersfancybrownhats.com",
+               "title"=>"View Website"
+             ],
+             [
+               "type"=>"postback",
+               "title"=>"Start Chatting",
+               "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+             ]              
+           ]
+         ]
+       ]
+     ]
+   ]];
+
+    $response = [
+   'recipient' => [ 'id' => $senderId ],
+   'message' => $answer 
+];}
+if($messageText == "more") {
+
+    $answer = ["attachment"=>[
+        "type"=>"template",
+        "payload"=>[
+          "template_type"=>"button",
+          "text"=>"What do you want to do next?",
+          "buttons"=>[
+            [
+              "type"=>"web_url",
+              "url"=>"https://petersapparel.parseapp.com",
+              "title"=>"Show Website"
+            ],
+            [
+              "type"=>"postback",
+              "title"=>"Start Chatting",
+              "payload"=>"USER_DEFINED_PAYLOAD"
+            ]
+          ]
+        ]
+        ]];
+  
+        $response = [
+      'recipient' => [ 'id' => $senderId ],
+      'message' => $answer
+  ];
+  }
 
 $response = [
     'recipient' => [ 'id' => $senderId ],
