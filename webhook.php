765ff02e1ad9495796fae3e4672c26d8d5a1301b
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 10:51:57 +0700

    now

--- webhook.php
+++ webhook.php
@@ -18,7 +18,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
     $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
 
-    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_ACCESS_TOKEN';
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=0e1b0adb5ae8269b9f55e5ac9a20dcdd';
 
     /*initialize curl*/
     $ch = curl_init($url);
