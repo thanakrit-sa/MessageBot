58344d8a76ad02d802c3a9d8a10f23d679e94543
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 13:44:40 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,24 +1,23 @@
 <?php
  include 'config.php';
-// check token at setup
 if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
   exit;
 }
 
-// handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
-
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 
-//set Message
-if($messageText == "hi") {
-    $answer = "Hello";
+
+if($messageText == "สวัสดีครับ") {
+    $answer = "สวัสดี";
+}
+else {
+    $answer = "ไม่เข้าใจ";
 }
 
-//send message to facebook bot
 $response = [
     'recipient' => [ 'id' => $senderId ],
     'message' => [ 'text' => $answer ]
