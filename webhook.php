f97c96f207271a8e211127ad4d69ece584f38c45
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-02 14:26:04 +0700

    now

--- webhook.php
+++ webhook.php
@@ -10,38 +10,33 @@ $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 
-if($messageText == "สวัสดีครับ") {
-
+if($messageText == "blog"){
     $answer = ["attachment"=>[
-        "type"=>"template",
-        "payload"=>[
-          "template_type"=>"button",
-          "text"=>"ต้องการติดต่อเรื่องอะไร ?",
-          "buttons"=>[
-            [
-              "type"=>"postback",
-              "title"=>"เปิด Account",
-              "payload"=>"USER_DEFINED_PAYLOAD"
-            ],
-            [
-                "type"=>"postback",
-                "title"=>"แจ้งปัญหา",
-                "payload"=>"USER_DEFINED_PAYLOAD"
-            ],
-            [
-                "type"=>"postback",
-                "title"=>"ติดต่อ",
-                "payload"=>"USER_DEFINED_PAYLOAD"
-            ],
-            [
-              "type"=>"web_url",
-              "url"=>"https://petersapparel.parseapp.com",
-              "title"=>"รายละเอียดเพิ่มเติม"
-            ]
-            
-          ]
-        ]
-        ]];
+     "type"=>"template",
+     "payload"=>[
+       "template_type"=>"generic",
+       "elements"=>[
+         [
+           "title"=>"Welcome to Peter\'s Hats",
+           "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+           "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+           "subtitle"=>"We\'ve got the right hat for everyone.",
+           "buttons"=>[
+             [
+               "type"=>"web_url",
+               "url"=>"https://petersfancybrownhats.com",
+               "title"=>"View Website"
+             ],
+             [
+               "type"=>"postback",
+               "title"=>"Start Chatting",
+               "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+             ]              
+           ]
+         ]
+       ]
+     ]
+   ]];
 }
 
 $response = [
