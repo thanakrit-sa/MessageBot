105f647437a84d74d9668c04d12d36f98d608d80
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-03 12:13:59 +0700

    now

--- webhook.php
+++ webhook.php
@@ -10,64 +10,85 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 
 
 $input = json_decode(file_get_contents('php://input'), true);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$response = null;
 
+if ($messageText == "เริ่มต้นใช้งาน") {
+    $answer = ["attachment" => [
+        "type" => "template",
+        "payload" => [
+            "template_type" => "generic",
+            "elements" => [
+                [
+                    "title" => "ยินดีต้องรับ",
+                    "item_url" => "https://www.google.com/?hl=th",
+                    "image_url" => "https://i0.wp.com/dgcasino.com/wp-content/uploads/2019/01/%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%9A%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B9%88%E0%B8%B2-dg-casino.jpg?resize=596%2C300",
+                    "subtitle" => "กรุณาเลือกหัวข้อที่ต้องการ",
+                    "buttons" => [
+                        [
+                            "type" => "postback",
+                            "title" => "เปิด Account",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
+                        [
+                            "type" => "postback",
+                            "title" => "แจ้งปัญหา",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
+                        [
+                            "type" => "postback",
+                            "title" => "ติดต่อ",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
 
-if (!is_null($input)) {
-    $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-    $typeMessage = $input['entry'][0]['message']['type'];
-    $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-    $response = null;
-    switch ($typeMessage) {
-        case 'text':
-        default:
-            $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-            $response = [
-                'recipient' => ['id' => $senderId],
-                'message' => $textReplyMessage
-            ];
-            break;
-    }
+                    ],
+
+                ]
+            ]
+        ]
+    ]];
 }
-// if ($messageText == "เริ่มต้นใช้งาน") {
-//     $answer = ["attachment" => [
-//         "type" => "template",
-//         "payload" => [
-//             "template_type" => "generic",
-//             "elements" => [
-//                 [
-//                     "title" => "ยินดีต้องรับ",
-//                     "item_url" => "https://www.google.com/?hl=th",
-//                     "image_url" => "https://i0.wp.com/dgcasino.com/wp-content/uploads/2019/01/%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%9A%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B9%88%E0%B8%B2-dg-casino.jpg?resize=596%2C300",
-//                     "subtitle" => "กรุณาเลือกหัวข้อที่ต้องการ",
-//                     "buttons" => [
-//                         [
-//                             "type" => "postback",
-//                             "title" => "เปิด Account",
-//                             "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-//                         ],
-//                         [
-//                             "type" => "postback",
-//                             "title" => "แจ้งปัญหา",
-//                             "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-//                         ],
-//                         [
-//                             "type" => "postback",
-//                             "title" => "ติดต่อ",
-//                             "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-//                         ],
+else if($messageText != "เริ่มต้นใช้งาน") {
+    $answer = ["attachment" => [
+        "type" => "template",
+        "payload" => [
+            "template_type" => "generic",
+            "elements" => [
+                [
+                    "title" => "ยินดีต้องรับ",
+                    "item_url" => "https://www.google.com/?hl=th",
+                    "image_url" => "https://i0.wp.com/dgcasino.com/wp-content/uploads/2019/01/%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%9A%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B9%88%E0%B8%B2-dg-casino.jpg?resize=596%2C300",
+                    "subtitle" => "กรุณาเลือกหัวข้อที่ต้องการ",
+                    "buttons" => [
+                        [
+                            "type" => "postback",
+                            "title" => "เปิด Account",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
+                        [
+                            "type" => "postback",
+                            "title" => "แจ้งปัญหา",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
+                        [
+                            "type" => "postback",
+                            "title" => "ติดต่อ",
+                            "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                        ],
 
-//                     ],
+                    ],
 
-//                 ]
-//             ]
-//         ]
-//     ]];
-// }
+                ]
+            ]
+        ]
+    ]];
+}
 
-// $response = [
-//     'recipient' => ['id' => $senderId],
-//     'message' => $answer
-// ];
+$response = [
+    'recipient' => ['id' => $senderId],
+    'message' => $answer
+];
 
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . $accessToken);
 
@@ -78,3 +99,4 @@ if (!empty($input)) {
     $result = curl_exec($ch);
 }
 curl_close($ch);
+
