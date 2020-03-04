63b36a855840e10d1f88be06a55f3e95ea0856c8
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-04 10:15:04 +0700

    now

--- webhook.php
+++ webhook.php
@@ -15,7 +15,7 @@ $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
 $response = null;
 
-if (strpos($messageText,"บัญชี")  == true) {
+if (strpos($messageText,"บัญชี")  == true && strpos($messageText,"[yP=u")  == true) {
     $answer = ["attachment" => [
         "type" => "template",
         "payload" => [
@@ -50,74 +50,42 @@ if (strpos($messageText,"บัญชี")  == true) {
         ]
     ]];
 }
-else if ($messageText !== null) {
-    $answer = ["attachment" => [
-        "type" => "vertical",
-        "tag" => "generic",
-        "elements" => [
-          [
-            "type" => "vertical",
-            "elements" => [
+if (strpos($messageText,"ปัญหา")  == true && strpos($messageText,"xyPsk")  == true) {
+  $answer = ["attachment" => [
+      "type" => "template",
+      "payload" => [
+          "template_type" => "generic",
+          "elements" => [
               [
-                "type" => "image",
-                "url" => "https://i.pinimg.com/736x/a0/67/5e/a0675e5161d7ae5be2550987f397a641--flower-shops-paper-flowers.jpg",
-                "tooltip" => "Flowers"
-              ],
-              [
-                "type" => "text",
-                "tag" => "title",
-                "text" => "Birthday Bouquet",
-                "tooltip" => "Title"
-              ],
-              [
-                "type" => "text",
-                "tag" => "subtitle",
-                "text" => "Wild flowers",
-                "tooltip" => "subtitle"
-              ],
-              [
-                "type" => "button",
-                "tooltip" => "publish text example",
-                "title" => "publish text example",
-                "click" => [
-                  "actions" => [
-                    [
-                      "type" => "publishText",
-                      "text" => "published text button tap"
-                    ]
-                  ]
-                ]
-              ],
-              [
-                "type" => "button",
-                "tooltip" => "URL button example",
-                "title" => "URL button example",
-                "click" => [
-                  "actions" => [
-                    [
-                      "type" => "link",
-                      "name" => "URL button tap",
-                      "uri" => "https://www.pinterest.com/lyndawhite/beautiful-flowers/"
-                    ]
-                  ]
-                ]
-              ],
-              [
-                "type" => "button",
-                "title" => "Navigate",
-                "click" => [
-                     "actions" => [
-                    [
-                      "type" => "navigate",
-                      "lo" => 40.7562,
-                      "la" => -73.99861
-                    ]
-                  ]
-                ]
+                  "title" => "เปิดบัญชี",
+                  "item_url" => "https://www.google.com/?hl=th",
+                  "image_url" => "",
+                  "subtitle" => "กรุณาเลือกหัวข้อที่ต้องการ",
+                  "buttons" => [
+                      [
+                          "type" => "postback",
+                          "title" => "หัวข้อที่ 1",
+                          "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                      ],
+                      [
+                          "type" => "postback",
+                          "title" => "หัวข้อที่ 2",
+                          "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                      ],
+                      [
+                          "type" => "postback",
+                          "title" => "หัวข้อที่ 3",
+                          "payload" => "DEVELOPER_DEFINED_PAYLOAD"
+                      ],
+
+                  ],
+
               ]
-            ]
-          ]]]];
+          ]
+      ]
+  ]];
 }
+
 $response = [
     'recipient' => ['id' => $senderId],
     'message' => $answer
