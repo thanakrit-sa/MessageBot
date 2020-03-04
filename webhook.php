8892cd25d37edb344be4dfe6fdad9f18ba665f53
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-03-04 11:03:56 +0700

    now

--- webhook.php
+++ webhook.php
@@ -86,39 +86,7 @@ if ($messageText != null) {
     ]];
   }
 } else {
-  $answer = ["attachment" => [
-    "type" => "template",
-    "payload" => [
-      "template_type" => "generic",
-      "elements" => [
-        [
-          "title" => "เปิดบัญชี",
-          "item_url" => "https://www.google.com/?hl=th",
-          "image_url" => "",
-          "subtitle" => "กรุณาเลือกหัวข้อที่ต้องการ",
-          "buttons" => [
-            [
-              "type" => "postback",
-              "title" => "หัวข้อที่ 1",
-              "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-            ],
-            [
-              "type" => "postback",
-              "title" => "หัวข้อที่ 2",
-              "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-            ],
-            [
-              "type" => "postback",
-              "title" => "หัวข้อที่ 3",
-              "payload" => "DEVELOPER_DEFINED_PAYLOAD"
-            ],
-
-          ],
-
-        ]
-      ]
-    ]
-  ]];
+  echo "Error";
 }
 
 
