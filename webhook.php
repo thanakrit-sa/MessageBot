b78e81fd4c7c063a511f19583960db67dac406ca
thanakrit <iamsuttichai@Macbook-iamsuttichai.local>
2020-04-10 14:02:23 +0700

    now

--- webhook.php
+++ webhook.php
@@ -390,6 +390,57 @@ if ($message == "reply") {
         }
       }
     }';
+} else {
+  $jasonData = '{
+    {
+      "persistent_menu":[
+        {
+          "locale":"default",
+          "composer_input_disabled": true,
+          "call_to_actions":[
+            {
+              "title":"My Account",
+              "type":"nested",
+              "call_to_actions":[
+                {
+                  "title":"Pay Bill",
+                  "type":"postback",
+                  "payload":"PAYBILL_PAYLOAD"
+                },
+                {
+                  "title":"History",
+                  "type":"postback",
+                  "payload":"HISTORY_PAYLOAD"
+                },
+                {
+                  "title":"Contact Info",
+                  "type":"postback",
+                  "payload":"CONTACT_INFO_PAYLOAD"
+                }
+              ]
+            },
+            {
+              "type":"web_url",
+              "title":"Latest News",
+              "url":"http://www.messenger.com/",
+              "webview_height_ratio":"full"
+            }
+          ]
+        },
+        {
+          "locale":"zh_CN",
+          "composer_input_disabled":false,
+          "call_to_actions":[
+            {
+              "title":"Pay Bill",
+              "type":"postback",
+              "payload":"PAYBILL_PAYLOAD"
+            }
+          ]    
+        }
+      ]
+    }
+  }';
 }
 
 
