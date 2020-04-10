4de57128b9ebeb9429d5d7bcb0c0d11e290fcb41
thanakrit <iamsuttichai@Macbook-iamsuttichai.local>
2020-04-10 11:02:23 +0700

    now

--- webhook.php
+++ webhook.php
@@ -365,13 +365,30 @@ if ($message == "reply") {
       "attachment":{
         "type":"template",
         "payload":{
-          "template_type":"button",
-          "text":"What do you want to do next?",
-          "buttons":[
-            {
-              "type":"web_url",
-              "url":"https://www.messenger.com",
-              "title":"Visit Messenger"
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://petersfancybrownhats.com/view?item=103",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://petersfancybrownhats.com/"
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
             }
           ]
         }
