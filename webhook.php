b85226003f81c162446916fb350a48eb7f31cc0c
thanakrit <iamsuttichai@Macbook-iamsuttichai.local>
2020-04-10 14:15:40 +0700

    now

--- webhook.php
+++ webhook.php
@@ -392,40 +392,32 @@ if ($message == "reply") {
     }';
 } else {
   $jasonData = '{
-    "persistent_menu":[
-      {
-      "locale":"default",
-      "composer_input_disabled":true,
-      "call_to_actions":[
+    {
+      "persistent_menu": [
           {
-          "title":"Info",
-          "type":"nested",
-          "call_to_actions":[
-              {
-              "title":"Help",
-              "type":"postback",
-              "payload":"HELP_PAYLOAD"
-              },
-              {
-              "title":"Contact Me",
-              "type":"postback",
-              "payload":"CONTACT_INFO_PAYLOAD"
-              }
-          ]
-          },
-          {
-          "type":"web_url",
-          "title":"Visit website ",
-          "url":"http://www.techiediaries.com",
-          "webview_height_ratio":"full"
+              "locale": "default",
+              "composer_input_disabled": false,
+              "call_to_actions": [
+                  {
+                      "type": "postback",
+                      "title": "Talk to an agent",
+                      "payload": "CARE_HELP"
+                  },
+                  {
+                      "type": "postback",
+                      "title": "Outfit suggestions",
+                      "payload": "CURATION"
+                  },
+                  {
+                      "type": "web_url",
+                      "title": "Shop now",
+                      "url": "https://www.originalcoastclothing.com/",
+                      "webview_height_ratio": "full"
+                  }
+              ]
           }
       ]
-      },
-      {
-      "locale":"zh_CN",
-      "composer_input_disabled":false
-      }
-  ]
+  }
   }';
 }
 
