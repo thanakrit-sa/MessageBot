d77d8387e983f775665602d7ec238420839505b7
thanakrit <iamsuttichai@Macbook-iamsuttichai.local>
2020-04-10 11:18:29 +0700

    now

--- webhook.php
+++ webhook.php
@@ -381,7 +381,67 @@ if ($message == "reply") {
                   "type":"postback",
                   "title":"Start Chatting",
                   "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                }              
+                },
+                {
+                  "title":"Welcome!",
+                  "image_url":"https://petersfancybrownhats.com/company_image.png",
+                  "subtitle":"We have the right hat for everyone.",
+                  
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://petersfancybrownhats.com",
+                      "title":"View Website"
+                    },{
+                      "type":"postback",
+                      "title":"Start Chatting",
+                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                    },
+                    {
+                      "title":"Welcome!",
+                      "image_url":"https://petersfancybrownhats.com/company_image.png",
+                      "subtitle":"We have the right hat for everyone.",
+                      
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"https://petersfancybrownhats.com",
+                          "title":"View Website"
+                        },{
+                          "type":"postback",
+                          "title":"Start Chatting",
+                          "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                        },
+                        {
+                          "title":"Welcome!",
+                          "image_url":"https://petersfancybrownhats.com/company_image.png",
+                          "subtitle":"We have the right hat for everyone.",
+                          
+                          "buttons":[
+                            {
+                              "type":"web_url",
+                              "url":"https://petersfancybrownhats.com",
+                              "title":"View Website"
+                            },{
+                              "type":"postback",
+                              "title":"Start Chatting",
+                              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                            },
+                            {
+                              "title":"Welcome!",
+                              "image_url":"https://petersfancybrownhats.com/company_image.png",
+                              "subtitle":"We have the right hat for everyone.",
+                              
+                              "buttons":[
+                                {
+                                  "type":"web_url",
+                                  "url":"https://petersfancybrownhats.com",
+                                  "title":"View Website"
+                                },{
+                                  "type":"postback",
+                                  "title":"Start Chatting",
+                                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                }                
               ]      
             }
           ]
@@ -389,7 +449,7 @@ if ($message == "reply") {
       }
     }
   }';
-  }
+}
 
 
 
