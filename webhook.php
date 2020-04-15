46805ff6da557b7c2550fc30641e00a433bd5f0d
thanakrit <thanakrit.10197@gmail.com>
2020-04-15 14:33:56 +0700

    now

--- webhook.php
+++ webhook.php
@@ -843,7 +843,6 @@ if ($message != null) {
                 "title":"ติดต่อผู้ดูแล",
                 webview_height_ratio: "tall",
                 messenger_extensions: true,
-                "fallback_url": "http://fe741861.ngrok.io/test/"
               },
               {
                 "type":"postback",
