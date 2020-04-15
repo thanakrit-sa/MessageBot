ab88a725c1a3322618dfd3f618c1a79c33513554
thanakrit <thanakrit.10197@gmail.com>
2020-04-15 13:55:29 +0700

    now

--- webhook.php
+++ webhook.php
@@ -28,6 +28,29 @@ $message = strtolower($message);
 // ----------------------------------------------------------------------------------------------------------------------------- Input
 // ----------------------------------------------------------------------------------------------------------------------------- Message
 
+function parse_signed_request($signed_request) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+  $secret = "appsecret"; // Use your app secret here
+
+  // Decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  // Confirm the signature
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+
 if ($message != null) {
   if ($message == "เมนูหลัก") {
     $jsonData = '{
