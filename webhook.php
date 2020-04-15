b991181d118236b9067763c88fb6b42f74d3e7ef
thanakrit <thanakrit.10197@gmail.com>
2020-04-15 09:52:37 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,21 +1,4 @@
-<script>
-  
 
-MessengerExtensions.requestCloseBrowser(function success() {
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
-}, function error(err) {
-  // an error occurred
-});
-</script>
 <?php
 
 
