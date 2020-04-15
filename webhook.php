45a8205581360759664eb409c966352b5e934a73
thanakrit <thanakrit.10197@gmail.com>
2020-04-15 13:37:51 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,4 +1,11 @@
 <script>
+(function (d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'Messenger'));
 
 app.post('/sendOnWebviewClose', (req, res) => {
   let psid = req.body.psid;
