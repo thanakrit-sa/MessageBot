f8bcb8a9dc95d5017035614cf9c2becb424480c9
thanakrit <thanakrit.10197@gmail.com>
2020-04-15 13:25:30 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,20 +1,4 @@
 <script>
- (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
-
-// MessengerExtensions.requestCloseBrowser(function success() {
-//   // webview closed
-// }, function error(err) {
-//   // an error occurred
-// });
 
 app.post('/sendOnWebviewClose', (req, res) => {
   let psid = req.body.psid;
