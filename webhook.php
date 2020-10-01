93c03c6ecda37ed37db700b85d92d77fc64232dd
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-10-01 10:22:15 +0700

    now

--- webhook.php
+++ webhook.php
@@ -25,13 +25,43 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
   $ch = curl_init($url);
   /*prepare response*/
   $jsonData = '{
-      "recipient":{
-        "id":"3564373453624541"
-      },
-      "message":{
-        "text":"hello, world!"
+    "recipient":{
+      "id":"<PSID>"
+    },
+    "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://petersfancybrownhats.com/view?item=103",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://petersfancybrownhats.com/"
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
+            }
+          ]
+        }
       }
-    }';
+    }
+  }';
   /* curl setting to send a json post data */
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
