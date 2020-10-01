b512131225da997df683573230fc7a7b8937e032
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-10-01 10:19:08 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,44 +1,44 @@
 
 <?php
 
-/* validate verify token needed for setting up web hook */ 
-if (isset($_GET['hub_verify_token'])) { 
-    if ($_GET['hub_verify_token'] === 'aaa') {
-        echo $_GET['hub_challenge'];
-        return;
-    } else {
-        echo 'Invalid Verify Token';
-        return;
-    }
+/* validate verify token needed for setting up web hook */
+if (isset($_GET['hub_verify_token'])) {
+  if ($_GET['hub_verify_token'] === 'aaa') {
+    echo $_GET['hub_challenge'];
+    return;
+  } else {
+    echo 'Invalid Verify Token';
+    return;
+  }
 }
 
 /* receive and send messages */
 $input = json_decode(file_get_contents('php://input'), true);
 if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 
-    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
-    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
+  $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
+  $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
 
-    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADSvg5yW7UBAB0ZBReBydfiXS7EksFOdphUlxj9XA5KO6cUwZA0AoUmO9J8gbGmk4Rj8qodsNb46qq3E7qYLZA9jhdiQ9OFDiRvFl6XBKDitVqjXpfjMZB0VZCV2zZCLjemlT1g2IhdwD1NC1Co0cFuFiDpGQZBqFxZBnAZChRZCjZCYMeDPQJWogm';
+  $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADSvg5yW7UBAB0ZBReBydfiXS7EksFOdphUlxj9XA5KO6cUwZA0AoUmO9J8gbGmk4Rj8qodsNb46qq3E7qYLZA9jhdiQ9OFDiRvFl6XBKDitVqjXpfjMZB0VZCV2zZCLjemlT1g2IhdwD1NC1Co0cFuFiDpGQZBqFxZBnAZChRZCjZCYMeDPQJWogm';
 
-    /*initialize curl*/
-    $ch = curl_init($url);
-    /*prepare response*/
-    $jsonData = '{
-    "recipient":{
-        "id":"' . $sender . '"
-        },
-        "message":{
-            "text":"You said, ' . $sender . '"
-        }
+  /*initialize curl*/
+  $ch = curl_init($url);
+  /*prepare response*/
+  $jsonData = '{
+      "recipient":{
+        "id":"3564373453624541"
+      },
+      "message":{
+        "text":"hello, world!"
+      }
     }';
-    /* curl setting to send a json post data */
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    if (!empty($message)) {
-        $result = curl_exec($ch); // user will get the message
-    }
+  /* curl setting to send a json post data */
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  if (!empty($message)) {
+    $result = curl_exec($ch); // user will get the message
+  }
 }
 
 ?>
