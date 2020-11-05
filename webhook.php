0f045ffb737611fef3ec30d09ab55eb7b9b4998a
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-11-05 13:41:24 +0700

    now

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = "EAADSvg5yW7UBAOlAM97B4wgvIPPIl7Xyct1X2otLFz5drI6jfKZCvoepcZBcFtxaOZAk7GZCFGJRJbvmYFLNjXHwwCcwZAM2TULPSZBF7sdZBEdIZAXCXYW25YJNSMyuOlSZBhmIbZC91cab05cS59vCdQWy5aUr9cQ4JDPQVDoMRvvAZDZD";
-$verify_token = "yellow_duck";
+$verify_token = "test";
 $hub_verify_token = null;
 if(isset($_REQUEST['hub_challenge'])) {
  $challenge = $_REQUEST['hub_challenge'];
@@ -13,34 +13,21 @@ $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 $message_to_reply = '';
-/**
- * Some Basic rules to validate incoming messages
- */
 
-
-//API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-//Initiate cURL.
 $ch = curl_init($url);
-//The JSON data.
 $jsonData = '{
     "recipient":{
         "id":"'.$sender.'"
     },
     "message":{
-        "text":"aaaa"
+        "text":"'.$sender.'"
     }
 }';
-//Encode the array into JSON.
 $jsonDataEncoded = $jsonData;
-//Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
-//Attach our encoded JSON string to the POST fields.
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-//Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
 }
