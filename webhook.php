02e542437d95d3efcbb8c82ba0c412bd2393ef33
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-11-05 13:44:14 +0700

    now

--- webhook.php
+++ webhook.php
@@ -21,7 +21,30 @@ $jsonData = '{
         "id":"'.$sender.'"
     },
     "message":{
-        "text":"'.$sender.'"
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
+            }
+          ]
+        }
+      }
     }
 }';
 $jsonDataEncoded = $jsonData;
